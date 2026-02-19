7fd75b295c70212e61bc6186dc45aae1ab279f00
Jimin Ha <jimin.ha@intel.com>
2026-02-18 20:30:23 -0800

    Replace HPUAttention with HPUMMEncoderAttention for Qwen3-VL model

--- vllm_gaudi/__init__.py
+++ vllm_gaudi/__init__.py
@@ -31,6 +31,7 @@ def register_ops():
 
 
 def register_models():
+    import vllm_gaudi.models.utils  # noqa: F401
     import vllm_gaudi.models.interfaces  # noqa: F401
     from .models import register_model
     register_model()
